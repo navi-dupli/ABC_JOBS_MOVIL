a2821df0f2424501a69d565a2346dfe969adbdd7
mviviana <m.ojedan@uniandes.edu.co>
2023-11-08 08:45:49 -0500

    Abcj 95 ver resultado entrevista (#22)

--- src/app/appointments/appointments-routing.module.ts
+++ src/app/appointments/appointments-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from '../guard/auth-guard.service';
 import { ListAppointmentsComponent } from './list-appointments/list-appointments.component';
+import { InterviewDetailComponent } from './interview-detail/interview-detail.component';
 
 const routes: Routes = [
   {
     path: 'listar-citas',
     component: ListAppointmentsComponent,
     canActivate: [AuthGuardService],
-  }
+  },
+  {
+    path: 'detalle-entrevista/:id',
+    component: InterviewDetailComponent
+  },
 ];
 
 @NgModule({
